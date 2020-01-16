cd434628ba5ee0eb259948c03171687cc1b3a2c3
Moritz <m.kaiser94@googlemail.com>
2020-01-16 16:10:09 +0100

    Add basic tests

---
+++ tests/server.test.js
@@ -0,0 +1,143 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const { seedDatabase } = require('./../seed/seed');
+
+const valideNewDatapoint = {
+    "id": 1234,
+    "city": "New City",
+    "start_date": "10/10/2010",
+    "end_date": "10/11/2010",
+    "price": "3.4",
+    "status": "Weekly",
+    "color": "#ababab"
+};
+
+const invalidDatapointWithMissingField = {
+    "city": "New City",
+    "start_date": "10/10/2010",
+    "end_date": "10/11/2010",
+    "price": "3.4",
+    "status": "Weekly",
+    "color": "#ababab"
+};
+
+const invalidDatapoint = {
+    "id": 1234,
+    "city": "",
+    "start_date": "10/10/2010",
+    "end_date": "10/11/2010",
+    "price": "3.4",
+    "status": "Weekly",
+    "color": "#ababab"
+};
+
+beforeEach(seedDatabase);
+
+describe('GET /', () => {
+    it('should return all datapoints', (done) => {
+        request(app)
+            .get('/')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.length).toBe(1000);
+            })
+            .end((err, res) => {
+                if(err) {
+                    return done(err);
+                }
+                done();
+            });
+    });
+});
+
+describe('POST /add', () => {
+    it('should add a datapoint', (done) => {
+        request(app)
+            .post('/add')
+            .send(valideNewDatapoint)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.id).toBe(valideNewDatapoint.id);
+            })
+            .end(done);
+    });
+
+    it('should reject empty body', (done) => {
+        request(app)
+            .post('/add')
+            .send({})
+            .expect(400)
+            .end(done);
+    });
+
+    it('should reject invalid datapoint with missing field', (done) => {
+        request(app)
+            .post('/add')
+            .send(invalidDatapointWithMissingField)
+            .expect(400)
+            .end(done);
+    });
+
+    it('should reject invalid datapoint', (done) => {
+        request(app)
+            .post('/add')
+            .send(invalidDatapoint)
+            .expect(400)
+            .end(done);
+    });
+});
+
+describe('POST /update/:id', () => {
+    it('should update a datapoint', (done) => {
+        request(app)
+            .post('/update/1')
+            .send(valideNewDatapoint)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.id).toBe(valideNewDatapoint.id);
+            })
+            .end(done);
+    });
+
+    it('should reject empty body', (done) => {
+        request(app)
+            .post('/update/1')
+            .send({})
+            .expect(400)
+            .end(done);
+    });
+
+    it('should reject invalid datapoint with missing field', (done) => {
+        request(app)
+            .post('/update/1')
+            .send(invalidDatapointWithMissingField)
+            .expect(400)
+            .end(done);
+    });
+
+    it('should reject invalid datapoint', (done) => {
+        request(app)
+            .post('/update/1')
+            .send(invalidDatapoint)
+            .expect(400)
+            .end(done);
+    });
+});
+
+describe('GET /delete/:id', () => {
+    it('should delete a datapoint', (done) => {
+        request(app)
+            .get('/delete/1')
+            .expect(200)
+            .end(done);
+    });
+
+    it('should reject invalid id', (done) => {
+        request(app)
+            .get('/delete/5291')
+            .expect(400)
+            .end(done);
+    });
+});
